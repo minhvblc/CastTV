013678f774983baf363d507d0f7bd6f1cfb7c84f
Minh Nguyễn <ngducminhvblc@gmail.com>
2023-06-13 16:18:34 +0700

    fixing

--- Package.swift
+++ Package.swift
@@ -23,9 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CastAndroid",
-            dependencies: []),
-        .testTarget(
-            name: "CastAndroidTests",
-            dependencies: ["CastAndroid"]),
+            dependencies: [])
     ]
 )
